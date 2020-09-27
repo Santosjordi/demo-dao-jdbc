4a2c78d7dd2a84e9b0aef0b64b9c65ebdb857e9a
Saint Jordi <jordiws@gmail.com>
2020-09-27 18:44:17 -0300

    Adicionada Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Book");
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Bobo Dakes", "bob.d@gmail.com", new Date(), 3000.00, obj);
 		
+		System.out.println(seller);
 	}
-
 }
