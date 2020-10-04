90639307cb2dce730ca803e534922b7a66cc8a0d
Saint Jordi <jordiws@gmail.com>
2020-10-04 18:50:11 -0300

    delete implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,21 @@
 package application;
 
+import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		System.out.println("==== Test: seller findById =======");
-		Seller seller = sellerDao.findById(3);
 		
-		System.out.println("\n==== Test5: seller update =======");
-		seller = sellerDao.findById(1);
-		seller.setName("Martha Wayne");
-		sellerDao.update(seller);
-		System.out.println("Inserted! New seller id: " + seller);
+		System.out.println("\n==== Test5: seller update =======\n Entre com um ID para deletar: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("deleted");
+		sc.close();
 	}
 }
--- src/model/dao/SellerDAO.java
+++ src/model/dao/SellerDAO.java
@@ -9,8 +9,8 @@ public interface SellerDao {
 
 	void insert(Seller obj);
 	void update(Seller obj);
-	void deleteById(Integer Id);
-	Seller findById(Integer Id);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
 	List<Seller> findAll();
 	List<Seller> findByDepartment(Department department);
 }
